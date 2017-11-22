fee3a2922149b2b21ec92b9a7fdc0db5e233176d
Olivia <oliviagelinas@icloud.com>
2017-11-22 13:57:33 -0500

    Add files via upload

---
+++ lab4-master/Lab3_Code_Source/Lab3Serveur/Lab3Serveur_H2012/main.cpp
@@ -0,0 +1,299 @@
+#undef UNICODE
+
+// IP 132.207.29.127
+
+#include <winsock2.h>
+#include <iostream>
+#include <algorithm>
+#include <strstream>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+// link with Ws2_32.lib
+#pragma comment( lib, "ws2_32.lib" )
+
+// External functions
+extern DWORD WINAPI EchoHandler(void* sd_) ;
+extern void DoSomething( char *src, char *dest );
+extern boolean validateCredentials(string username, string password);
+
+// List of Winsock error constants mapped to an interpretation string.
+// Note that this list must remain sorted by the error constants'
+// values, because we do a binary search on the list when looking up
+// items.
+static struct ErrorEntry {
+    int nID;
+    const char* pcMessage;
+
+    ErrorEntry(int id, const char* pc = 0) : 
+    nID(id), 
+    pcMessage(pc) 
+    { 
+    }
+
+    bool operator<(const ErrorEntry& rhs) const
+    {
+        return nID < rhs.nID;
+    }
+} gaErrorList[] = {
+    ErrorEntry(0,                  "No error"),
+    ErrorEntry(WSAEINTR,           "Interrupted system call"),
+    ErrorEntry(WSAEBADF,           "Bad file number"),
+    ErrorEntry(WSAEACCES,          "Permission denied"),
+    ErrorEntry(WSAEFAULT,          "Bad address"),
+    ErrorEntry(WSAEINVAL,          "Invalid argument"),
+    ErrorEntry(WSAEMFILE,          "Too many open sockets"),
+    ErrorEntry(WSAEWOULDBLOCK,     "Operation would block"),
+    ErrorEntry(WSAEINPROGRESS,     "Operation now in progress"),
+    ErrorEntry(WSAEALREADY,        "Operation already in progress"),
+    ErrorEntry(WSAENOTSOCK,        "Socket operation on non-socket"),
+    ErrorEntry(WSAEDESTADDRREQ,    "Destination address required"),
+    ErrorEntry(WSAEMSGSIZE,        "Message too long"),
+    ErrorEntry(WSAEPROTOTYPE,      "Protocol wrong type for socket"),
+    ErrorEntry(WSAENOPROTOOPT,     "Bad protocol option"),
+    ErrorEntry(WSAEPROTONOSUPPORT, "Protocol not supported"),
+    ErrorEntry(WSAESOCKTNOSUPPORT, "Socket type not supported"),
+    ErrorEntry(WSAEOPNOTSUPP,      "Operation not supported on socket"),
+    ErrorEntry(WSAEPFNOSUPPORT,    "Protocol family not supported"),
+    ErrorEntry(WSAEAFNOSUPPORT,    "Address family not supported"),
+    ErrorEntry(WSAEADDRINUSE,      "Address already in use"),
+    ErrorEntry(WSAEADDRNOTAVAIL,   "Can't assign requested address"),
+    ErrorEntry(WSAENETDOWN,        "Network is down"),
+    ErrorEntry(WSAENETUNREACH,     "Network is unreachable"),
+    ErrorEntry(WSAENETRESET,       "Net connection reset"),
+    ErrorEntry(WSAECONNABORTED,    "Software caused connection abort"),
+    ErrorEntry(WSAECONNRESET,      "Connection reset by peer"),
+    ErrorEntry(WSAENOBUFS,         "No buffer space available"),
+    ErrorEntry(WSAEISCONN,         "Socket is already connected"),
+    ErrorEntry(WSAENOTCONN,        "Socket is not connected"),
+    ErrorEntry(WSAESHUTDOWN,       "Can't send after socket shutdown"),
+    ErrorEntry(WSAETOOMANYREFS,    "Too many references, can't splice"),
+    ErrorEntry(WSAETIMEDOUT,       "Connection timed out"),
+    ErrorEntry(WSAECONNREFUSED,    "Connection refused"),
+    ErrorEntry(WSAELOOP,           "Too many levels of symbolic links"),
+    ErrorEntry(WSAENAMETOOLONG,    "File name too long"),
+    ErrorEntry(WSAEHOSTDOWN,       "Host is down"),
+    ErrorEntry(WSAEHOSTUNREACH,    "No route to host"),
+    ErrorEntry(WSAENOTEMPTY,       "Directory not empty"),
+    ErrorEntry(WSAEPROCLIM,        "Too many processes"),
+    ErrorEntry(WSAEUSERS,          "Too many users"),
+    ErrorEntry(WSAEDQUOT,          "Disc quota exceeded"),
+    ErrorEntry(WSAESTALE,          "Stale NFS file handle"),
+    ErrorEntry(WSAEREMOTE,         "Too many levels of remote in path"),
+    ErrorEntry(WSASYSNOTREADY,     "Network system is unavailable"),
+    ErrorEntry(WSAVERNOTSUPPORTED, "Winsock version out of range"),
+    ErrorEntry(WSANOTINITIALISED,  "WSAStartup not yet called"),
+    ErrorEntry(WSAEDISCON,         "Graceful shutdown in progress"),
+    ErrorEntry(WSAHOST_NOT_FOUND,  "Host not found"),
+    ErrorEntry(WSANO_DATA,         "No host data of that type was found")
+};
+const int kNumMessages = sizeof(gaErrorList) / sizeof(ErrorEntry);
+
+
+//// WSAGetLastErrorMessage ////////////////////////////////////////////
+// A function similar in spirit to Unix's perror() that tacks a canned 
+// interpretation of the value of WSAGetLastError() onto the end of a
+// passed string, separated by a ": ".  Generally, you should implement
+// smarter error handling than this, but for default cases and simple
+// programs, this function is sufficient.
+//
+// This function returns a pointer to an internal static buffer, so you
+// must copy the data from this function before you call it again.  It
+// follows that this function is also not thread-safe.
+const char* WSAGetLastErrorMessage(const char* pcMessagePrefix, int nErrorID = 0)
+{
+    // Build basic error string
+    static char acErrorBuffer[256];
+    ostrstream outs(acErrorBuffer, sizeof(acErrorBuffer));
+    outs << pcMessagePrefix << ": ";
+
+    // Tack appropriate canned message onto end of supplied message 
+    // prefix. Note that we do a binary search here: gaErrorList must be
+	// sorted by the error constant's value.
+	ErrorEntry* pEnd = gaErrorList + kNumMessages;
+    ErrorEntry Target(nErrorID ? nErrorID : WSAGetLastError());
+    ErrorEntry* it = lower_bound(gaErrorList, pEnd, Target);
+    if ((it != pEnd) && (it->nID == Target.nID)) {
+        outs << it->pcMessage;
+    }
+    else {
+        // Didn't find error in list, so make up a generic one
+        outs << "unknown error";
+    }
+    outs << " (" << Target.nID << ")";
+
+    // Finish error message off and return it.
+    outs << ends;
+    acErrorBuffer[sizeof(acErrorBuffer) - 1] = '\0';
+    return acErrorBuffer;
+}
+
+int main(void) 
+{
+	//----------------------
+	// Initialize Winsock.
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != NO_ERROR) {
+		cerr << "Error at WSAStartup()\n" << endl;
+		return 1;
+	}
+
+	//----------------------
+	// Create a SOCKET for listening for
+	// incoming connection requests.
+	SOCKET ServerSocket;
+	ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ServerSocket == INVALID_SOCKET) {
+        cerr << WSAGetLastErrorMessage("Error at socket()") << endl;
+		WSACleanup();
+		return 1;
+	}
+	char option[] = "1";
+	setsockopt(ServerSocket, SOL_SOCKET, SO_REUSEADDR, option, sizeof(option));
+
+    //----------------------
+    // The sockaddr_in structure specifies the address family,
+    // IP address, and port for the socket that is being bound.
+    //int port=5000;
+    
+	//Recuperation de l'adresse locale
+	hostent *thisHost;
+	
+	printf("server has started...\n");
+	bool validIp = false;
+	bool validPort = false;
+
+	string ip_input;
+	string port;
+	struct sockaddr_in serverService; 
+
+	struct sockaddr_in sa;
+
+
+	//use get_s instead of cin to avoid shenanigans
+	while (!validIp) {
+		printf("Enter your IP address: \n");
+		getline(std::cin, ip_input);
+		if (inet_pton(AF_INET, ip_input.c_str(), &(sa.sin_addr)) == 1) {
+			validIp = true;
+		}
+	}
+
+	while (!validPort) {
+		printf("Enter your port address: \n");
+		getline(std::cin, port);
+		if (stoi(port) >= 5000 && stoi(port) <= 5050) {
+			validPort = true;
+		}
+	}
+	thisHost=gethostbyname(ip_input.c_str());
+	char* ip;
+	ip=inet_ntoa(*(struct in_addr*) *thisHost->h_addr_list);
+	printf("Adresse locale trouvee %s : \n\n",ip);
+	sockaddr_in service;
+    service.sin_family = AF_INET;
+    //service.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//	service.sin_addr.s_addr = INADDR_ANY;
+	service.sin_addr.s_addr = inet_addr(ip_input.c_str());
+    service.sin_port = htons(stoi(port));
+
+    if (bind(ServerSocket, (SOCKADDR*) &service, sizeof(service)) == SOCKET_ERROR) {
+		cerr << WSAGetLastErrorMessage("bind() failed.") << endl;
+		closesocket(ServerSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	//----------------------
+	// Listen for incoming connection requests.
+	// on the created socket
+	if (listen(ServerSocket, 30) == SOCKET_ERROR) {
+		cerr << WSAGetLastErrorMessage("Error listening on socket.") << endl;
+		closesocket(ServerSocket);
+		WSACleanup();
+		return 1;
+	}
+
+
+	printf("En attente des connections des clients sur le port %d...\n\n",ntohs(service.sin_port));
+
+    while (true) {	
+
+		sockaddr_in sinRemote;
+		 int nAddrSize = sizeof(sinRemote);
+		// Create a SOCKET for accepting incoming requests.
+		// Accept the connection.
+		 SOCKET sd = accept(ServerSocket, (sockaddr*)&sinRemote, &nAddrSize);
+        if (sd != INVALID_SOCKET) {
+			cout << "Connection acceptee De : " <<
+                    inet_ntoa(sinRemote.sin_addr) << ":" <<
+                    ntohs(sinRemote.sin_port) << "." <<
+                    endl;
+
+            DWORD nThreadID;
+            CreateThread(0, 0, EchoHandler, (void*)sd, 0, &nThreadID);
+        }
+        else {
+            cerr << WSAGetLastErrorMessage("Echec d'une connection.") << 
+                    endl;
+           // return 1;
+        }
+    }
+}
+
+
+//// EchoHandler ///////////////////////////////////////////////////////
+// Handles the incoming data by reflecting it back to the sender.
+
+DWORD WINAPI EchoHandler(void* sd_) 
+{
+	//132.207.29.127
+	SOCKET sd = (SOCKET)sd_;
+
+	// Read Data from client
+		char readBuffer[200], outBuffer[200];
+		int readBytes;
+
+		readBytes = recv(sd, readBuffer, 200, 0);
+		while (readBytes > 0) {
+			cout << "Received " << readBytes << " bytes from client." << endl;
+			cout << "Received " << readBuffer << " from client." << endl;
+			if (string(readBuffer).find("$username=") != std::string::npos) {
+				validateCredentials(string(readBuffer).substr(string(readBuffer).find("$username=", string(readBuffer).length())),
+					"PASSWORD HERE");
+				memset(readBuffer, 0, sizeof readBuffer);
+			}
+			DoSomething(readBuffer, outBuffer);
+			send(sd, outBuffer, readBytes, 0);
+			readBytes = recv(sd, readBuffer, 200, 0);
+		}
+		if (readBytes == SOCKET_ERROR) {
+			cout << WSAGetLastErrorMessage("Echec de la reception !") << endl;
+		}
+	
+	//closesocket(sd);
+
+	return 0;
+}
+// Do Something with the information
+void DoSomething( char *src, char *dest )
+{
+	for (auto i = 0; i < 200; i++ ) {
+		dest[i] = (i%2 == 0) ? src[i] : toupper(src[i]);
+	}
+}
+
+boolean validateCredentials(string username, string password)
+{
+	//to do 
+	return false;
+}
+
